46e6e851fd976f45c82c568dcd89528c0eff6d1d
Parash Bista <parasbista18@gmail.com>
2025-10-16 11:05:05 +0545

    Update ALLOWED_HOSTS to include subdomains
    Added support for all subdomains of onrender.com.

--- expense_tracker_new/settings.py
+++ expense_tracker_new/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-cv=-+$noa6m#r9)lfi_$%au7!#avo=lbf99rnfeqb3k@@4pvj@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'expense-tracker-new-wow3.onrender.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'expense-tracker-new-wow3.onrender.com',
+    '.onrender.com'   # This allows any subdomain of onrender.com
+]
 
 
 # Application definition
